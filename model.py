a2284de45277995bb339299d5c8e822d22f4ce04
dennisad <dennisad@stud.ntnu.no>
2021-03-13 18:58:36 +0100

    unet

--- model.py
+++ model.py
@@ -8,13 +8,12 @@ import tensorflow as tf
 from tensorflow.keras.layers import *
 from tensorflow.keras import Model
 
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
 print(os.getcwd())
-
 
 data_path = r'hackathon_kpis_anonymised.csv'
 df = pd.read_csv(data_path)
@@ -22,194 +21,202 @@ df_orig = df
 
 df = df.dropna()
 df = df.values
-df = df[:,2:]
+df = df[:, 2:]
 
-#%%
+
 data_path_pred = r"predicted_nans.csv"
 df_pred = pd.read_csv(data_path_pred)
 df_pred = df_pred.values
 
 assert df_pred.shape[-1] == df.shape[-1]
 
-df = np.vstack((df,df_pred))
+df = np.vstack((df, df_pred))
 print(df.shape)
 
-#%%
 
-def build_model(input_shape,ksize=3):
-    
-    #encoder
-    conv1 = Conv1D(input_shape.shape[-1],ksize,activation='relu',padding='same')((input_shape))
+
+def build_model(input_shape, ksize=3):
+    # encoder
+    conv1 = Conv1D(input_shape.shape[-1], ksize, activation='relu', padding='same')((input_shape))
     pool1 = MaxPooling1D(pool_size=2)(conv1)
-    conv2 = conv1 = Conv1D(64,ksize,activation='relu',padding='same')(pool1)
+    conv2 = conv1 = Conv1D(64, ksize, activation='relu', padding='same')(pool1)
     pool2 = MaxPooling1D(pool_size=2)(conv2)
-    conv3 = Conv1D(128,ksize,activation='relu',padding='same')(pool2)
-    
-    #decoder
-    conv4 = Conv1D(128,ksize,activation='relu',padding='same')(conv3)
+    conv3 = Conv1D(128, ksize, activation='relu', padding='same')(pool2)
+
+    # decoder
+    conv4 = Conv1D(128, ksize, activation='relu', padding='same')(conv3)
     up1 = UpSampling1D(2)(conv4)
-    conv5 = Conv1D(64,ksize,activation='relu',padding='same')(up1)
+    conv5 = Conv1D(64, ksize, activation='relu', padding='same')(up1)
     up2 = UpSampling1D(2)(conv5)
-    out = Conv1D(input_shape.shape[-1],ksize,activation='sigmoid',padding='same')(up2)
-    
+    out = Conv1D(input_shape.shape[-1], ksize, activation='sigmoid', padding='same')(up2)
+
     return out
 
 
-
-
-#%%
-
+# %%
 
 
 window_size = 32
 
-
-
 a = df.shape[0] % window_size
-xtrain = df[:-a,:]
-xtrain = xtrain.reshape(xtrain.shape[0]//window_size,window_size,xtrain.shape[1])
+xtrain = df[:-a, :]
+xtrain = xtrain.reshape(xtrain.shape[0] // window_size, window_size, xtrain.shape[1])
 xtrain = xtrain.astype(np.single)
 
 original_data = xtrain
 
-#shuffler treningsdata
+# shuffler treningsdata
 np.random.shuffle(xtrain)
-#%%
+# %%
 
-#beholder en liten andel for testing
+# beholder en liten andel for testing
 test_split = 0.3
 xtest = xtrain[:int(xtrain.shape[0] * test_split)]
 
-#resterende brukes til trening
+# resterende brukes til trening
 xtrain = xtrain[int(xtrain.shape[0] * test_split):]
 
+input_shape = tf.keras.layers.Input(shape=(xtrain.shape[1], xtrain.shape[2]))
+
+# model = Model(input_shape, build_model(input_shape, ksize=9))
+# model.compile(loss='mse', optimizer='adam')
+# model.summary()
+
+# EPOCHS = 100
+
+# callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+
+# history = model.fit(xtrain, xtrain, validation_split=.3, epochs=EPOCHS, callbacks=[callback])
+
+# model.save("autoencoder_model.h5")
+# pd.DataFrame.from_dict(history.history).to_csv('history.csv', index=False)
+model = tf.keras.models.load_model("autoencoder_model.h5")
+history = pd.read_csv("history.csv")
 
 
-input_shape = tf.keras.layers.Input(shape=(xtrain.shape[1],xtrain.shape[2]))
+# model = tf.keras.models.load_model("autoencoder_model.h5")
+# history = model.fit(xtrain, xtrain, validation_split=.3, epochs=EPOCHS, callbacks=[callback])
+
+# HERIFRA OG NED BØR INN I EGNE FUNKJSNOER
+
+# plot training history
+def plotData(history):
+    plt.plot(history['val_loss'], label="validation_loss")
+    plt.plot(history['loss'], label="training_loss")
+    plt.legend()
+    plt.show()
 
 
-model = Model(input_shape,build_model(input_shape,ksize=9))
-model.compile(loss='mse',optimizer='adam')
-model.summary()
-
-EPOCHS = 100
-
-callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
-
-history = model.fit(xtrain,xtrain,validation_split=.3,epochs=EPOCHS,callbacks=[callback])
-#%%
-model.save("autoencoder_model.h5")
-
-#%%
-#HERIFRA OG NED BØR INN I EGNE FUNKJSNOER
-
-#plot training history
-plt.plot(history.history['val_loss'],label="validation_loss")
-plt.plot(history.history['loss'],label="training_loss")
-plt.legend()
-
-#%%
+plotData(history)
 
 
 
-#plot a single feature
 
-#henter labels fra dataframen
-labels = df_orig.columns[2:]
+# plot a single feature
+def plot_single_feature(df, model, num_plot=8, feature=0):
+    # henter labels fra dataframen
+    labels = df.columns[2:]
 
-#antallet du vil plotte (må være partall)
-num_to_plot = 8
+    # antallet du vil plotte (må være partall)
+
+    predictions = model.predict(xtest[:num_plot])
+
+    fig, axs = plt.subplots(2, num_plot // 2, sharey=True)
+
+    for ind, ax in enumerate(axs.flat):
+        ax.plot(predictions[ind, :, feature], label="pred")
+        ax.plot(xtest[ind, :, feature], label="gt")
+        ax.legend()
+
+    fig.canvas.set_window_title(labels[feature])
+    plt.show()
 
 
-predictions = model.predict(xtest[:num_to_plot])
+plot_single_feature(df_orig, model, 8, 0)
 
-fig,axs = plt.subplots(2,num_to_plot//2,sharey=True)
-feature = 3
 
-for ind,ax in enumerate(axs.flat):
-    ax.plot(predictions[ind,:,feature],label="pred")
-    ax.plot(xtest[ind,:,feature],label="gt")
-    ax.legend()
-    
-    
-fig.canvas.set_window_title(labels[feature])
+# plot SE over one feature
+def plotSE_feature(df, model):
+    labels = df.columns[2:]
 
-#%%
-#plot SE over one feature
-labels = df_orig.columns[2:]
+    num_to_plot = 8
 
-num_to_plot = 8
+    predictions = model.predict(xtest[:num_to_plot])
 
-predictions = model.predict(xtest[:num_to_plot])
+    fig, axs = plt.subplots(2, num_to_plot // 2, sharey=True)
+    feature = 3
 
-fig,axs = plt.subplots(2,num_to_plot//2,sharey=True)
-feature = 3
+    for ind, ax in enumerate(axs.flat):
+        ax.plot((xtest[ind, :, feature] - predictions[ind, :, feature]) ** 2, label="pred")
+        ax.legend()
 
-for ind,ax in enumerate(axs.flat):
-    ax.plot((xtest[ind,:,feature]-predictions[ind,:,feature])**2,label="pred")
-    ax.legend()
-    
-    
-fig.canvas.set_window_title(labels[feature])
+    fig.canvas.set_window_title(labels[feature])
+    plt.show()
 
-#%%
+
+plotSE_feature(df_orig, model)
+
+
+
 ##plot features
+def plot_features(df, model):
+    labels = df.columns[2:]
 
-labels = df_orig.columns[2:]
+    predictions = model.predict(xtest[0].reshape(1, xtest.shape[1], xtest.shape[-1]))
+    num_to_plot = predictions.shape[-1]
 
-predictions = model.predict(xtest[0].reshape(1,xtest.shape[1],xtest.shape[-1]))
-num_to_plot = predictions.shape[-1]
+    fig, axs = plt.subplots(num_to_plot // 2, 2, figsize=(10, 8))
 
-fig,axs = plt.subplots(num_to_plot//2,2,figsize=(10,8))
+    for ind, ax in enumerate(axs.flat):
+        ax.set_title(labels[ind])
+        ax.plot(predictions[0, :, ind], label="pred")
+        ax.plot(xtest[0, :, ind], label="gt")
 
-for ind,ax in enumerate(axs.flat):
-    ax.set_title(labels[ind])
-    ax.plot(predictions[0,:,ind],label="pred")
-    ax.plot(xtest[0,:,ind],label="gt")
-    
-
-fig.tight_layout()
-plt.legend()
-
-#%%
-#plot MSE over all features
-
-predictions = model.predict(xtest)
-predictions = predictions.reshape(predictions.shape[0]*predictions.shape[1],predictions.shape[-1])
-xtest_unravel = xtest.reshape(xtest.shape[0]*xtest.shape[1],xtest.shape[-1])
-
-n_features = predictions.shape[-1]
-
-predictions = np.sum(predictions,axis=-1)
-xtest_unravel = np.sum(xtest_unravel,axis=-1)
-
-mse = 1/n_features * (xtest_unravel - predictions)**2
-
-plt.scatter(np.arange(len(mse)),mse)
-
-#%%
-#plot MSE over all features, FULL DATASET
+    fig.tight_layout()
+    plt.legend()
+    plt.show()
 
 
-predictions = model.predict(original_data)
-predictions = predictions.reshape(predictions.shape[0]*predictions.shape[1],predictions.shape[-1])
-xtest_unravel = original_data.reshape(original_data.shape[0]*original_data.shape[1],original_data.shape[-1])
-
-n_features = predictions.shape[-1]
-
-predictions = np.sum(predictions,axis=-1)
-xtest_unravel = np.sum(xtest_unravel,axis=-1)
-
-mse = 1/n_features * (xtest_unravel - predictions)**2
-
-plt.scatter(np.arange(len(mse)),mse)
+plotSE_feature(df_orig, model)
 
 
 
+# plot MSE over all features
+def plot_MSE_features(df, model):
+    predictions = model.predict(xtest)
+    predictions = predictions.reshape(predictions.shape[0] * predictions.shape[1], predictions.shape[-1])
+    xtest_unravel = xtest.reshape(xtest.shape[0] * xtest.shape[1], xtest.shape[-1])
+
+    n_features = predictions.shape[-1]
+
+    predictions = np.sum(predictions, axis=-1)
+    xtest_unravel = np.sum(xtest_unravel, axis=-1)
+
+    mse = 1 / n_features * (xtest_unravel - predictions) ** 2
+
+    plt.scatter(np.arange(len(mse)), mse)
+    plt.show()
+
+
+plot_MSE_features(df, model)
 
 
 
+# plot MSE over all features, FULL DATASET
+def plot_MSE_feature_full(model):
+    predictions = model.predict(original_data)
+    predictions = predictions.reshape(predictions.shape[0] * predictions.shape[1], predictions.shape[-1])
+    xtest_unravel = original_data.reshape(original_data.shape[0] * original_data.shape[1], original_data.shape[-1])
+
+    n_features = predictions.shape[-1]
+
+    predictions = np.sum(predictions, axis=-1)
+    xtest_unravel = np.sum(xtest_unravel, axis=-1)
+
+    mse = 1 / n_features * (xtest_unravel - predictions) ** 2
+
+    plt.scatter(np.arange(len(mse)), mse)
+    plt.show()
 
 
-
-
+plot_MSE_feature_full(model)
--- model_generate_missing_nan.py
+++ model_generate_missing_nan.py
@@ -15,8 +15,12 @@ import numpy as np
 
 
 
-data_path = r'C:\Users\adelv\Documents\Hackaton\hackathon_kpis_anonymised\hackathon_kpis_anonymised.csv'
-df = pd.read_csv(data_path)
+data_path = r'hackathon_kpis_anonymised.csv'
+df = pd.read_csv(data_path,sep=",")
+
+df.sort_values(['cell_name', 'timestamp'], ascending=[True, False],inplace=True)
+print(df.columns)
+
 df2 = df
 df = df.dropna()
 df2 = df2.drop(df.index)
@@ -28,22 +32,22 @@ df2=df2.values[:,2:]
 df = df.values
 df = df[:,2:]
 
-#%%
+
 
 def build_model(input_shape,ksize=3):
     
     #encoder
-    conv1 = Conv1D(16,ksize,activation='relu',padding='same')((input_shape))
-    pool1 = MaxPooling1D(pool_size=2)(conv1)
+    conv1 = Conv1D(14,ksize,activation='relu',padding='same')(input_shape)
+    pool1 = MaxPooling1D(pool_size=1)(conv1)
     conv2 = conv1 = Conv1D(32,ksize,activation='relu',padding='same')(pool1)
-    pool2 = MaxPooling1D(pool_size=2)(conv2)
+    pool2 = MaxPooling1D(pool_size=1)(conv2)
     conv3 = Conv1D(64,ksize,activation='relu',padding='same')(pool2)
     
     #decoder
     conv4 = Conv1D(64,ksize,activation='relu',padding='same')(conv3)
-    up1 = UpSampling1D(2)(conv4)
+    up1 = UpSampling1D(1)(conv4)
     conv5 = Conv1D(32,ksize,activation='relu',padding='same')(up1)
-    up2 = UpSampling1D(2)(conv5)
+    up2 = UpSampling1D(1)(conv5)
     out = Conv1D(14,ksize,activation='sigmoid',padding='same')(up2)
     
     return out
@@ -55,20 +59,67 @@ def simple_autoencoder(input_shape):
     layer2 = Dense(7,activation='relu')(layer1)
     layer3 = Dense(7,activation='relu')(layer2)
     output = Dense(14,activation='relu')(layer3)
+    return output
+
+def simple_conv_autoencoder(input_shape,ksize=3):
+    #encoder
+    layer1 = Conv1D(14,ksize=3,activation='relu')(input_shape)
+    layer2 = Conv1D(28,ksize=3,activation='relu')(layer1)
+    layer3 = Conv1D(28,ksize=3,activation='relu')(layer2)
+    output = Dense(14,ksize=3,activation='relu')(layer3)
+    
     
     return output
     
     
-    
+def unet(input_shape,pretrained_weights = None,ksize=3):
+
+    conv1 = Conv1D(14, ksize, activation='relu', padding='same')(input_shape)
+    conv1 = Conv1D(14, ksize, activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling1D(pool_size=2)(conv1)
+
+    conv2 = Conv1D(32, ksize, activation='relu', padding='same')(pool1)
+    conv2 = Conv1D(32, ksize, activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling1D(pool_size=2)(conv2)
+
+    conv3 = Conv1D(64, ksize, activation='relu', padding='same')(pool2)
+    conv3 = Conv1D(64, ksize, activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling1D(pool_size=2)(conv3)
+
+    conv4 = Conv1D(128, ksize, activation='relu', padding='same')(pool3)
+    conv4 = Conv1D(128, ksize, activation='relu', padding='same')(conv4)
+
+    up5 = concatenate([UpSampling1D(size=2)(conv4), conv3], axis=-1)
+    conv5 = Conv1D(64, ksize, activation='relu', padding='same')(up5)
+    conv5 = Conv1D(64, ksize, activation='relu', padding='same')(conv5)
+
+    up6 = concatenate([UpSampling1D(size=2)(conv5), conv2], axis=-1)
+    conv6 = Conv1D(32, ksize, activation='relu', padding='same')(up6)
+    conv6 = Conv1D(32, ksize, activation='relu', padding='same')(conv6)
+
+    up7 = concatenate([UpSampling1D(size=2)(conv6), conv1], axis=-1)
+    conv7 = Conv1D(14, ksize, activation='relu', padding='same')(up7)
+    conv7 = Conv1D(14, ksize, activation='relu', padding='same')(conv7)
+
+    conv8 = Conv1D(14,1, activation='sigmoid')(conv7)
+
+    return conv8
 
 
 
 
-#%%
-df = df.astype(np.single)
-xtrain=df
-xtrain = xtrain.reshape(xtrain.shape[0],1,xtrain.shape[-1])
-#%%
+window_size=32
+
+#sjekk at antall rader går opp i row-context
+#assert df.shape[0] % window_size == 0
+a = df.shape[0] % window_size
+xtrain = df[:-a]
+
+
+xtrain = xtrain.astype(np.single)
+xtrain = xtrain.reshape(xtrain.shape[0] // window_size, window_size, xtrain.shape[-1])
+
+
 #shuffler treningsdata
 np.random.shuffle(xtrain)
 
@@ -85,7 +136,9 @@ xtrain = xtrain[int(xtrain.shape[0] * test_split):]
 input_shape = tf.keras.layers.Input(shape=(xtrain.shape[1],xtrain.shape[2]))
 
 
-model = Model(input_shape,simple_autoencoder(input_shape))
+model = Model(input_shape, unet(input_shape,ksize=1) )
+
+
 model.compile(loss='mse',optimizer='adam')
 model.summary()
 
@@ -93,33 +146,45 @@ EPOCHS = 100
 
 callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=6)
 model.fit(xtrain,xtrain,validation_split=.1,epochs=EPOCHS,callbacks=[callback])
-#%%
 
+
+
+#%%
+b = df2.shape[0] % window_size
 df2 = df2.astype(np.single)
-df2 = df2.reshape(df2.shape[0],1,df2.shape[-1])
+df2 = df2[:-b]
+
+df2 = df2.reshape(df2.shape[0]//window_size,window_size,df2.shape[-1])
 preds = model.predict(df2)
 
 
-preds = preds.reshape(preds.shape[0],preds.shape[-1])
+#preds = preds.reshape(preds.shape[0],preds.shape[-1])
 
-np.savetxt('predicted_nans.csv', preds, delimiter=',')
+test = preds[:,1,:]
+x,y = np.where(df2[:,1,:] == - 1)
+out = df2[:,1,:][x,y]
+out = test[x,y]
+
+np.savetxt('predicted_nans.csv', df2[:,1,:], delimiter=',')
+
+
+
+
 
 
 
 
 #%%
-df2 = df2.astype(np.single)
-df2=df2.reshape(df2.shape[0],1,df2.shape[-1])
-
 num_to_plot = 8
+
 
 predictions = model.predict(df2[:num_to_plot])
 
 fig,axs = plt.subplots(2,num_to_plot//2,sharey=True,figsize=(10,10))
 
 for ind,ax in enumerate(axs.flat):
-    ax.plot(predictions[ind,0,:],label="pred")
-    ax.plot(df2[ind,0,:],label="gt")
+    ax.plot(predictions[ind,:,2],label="pred")
+    ax.plot(df2[ind,:,2],label="gt")
     
     
 
